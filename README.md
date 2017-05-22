f717b54f99622fa3ae21b53fd30dbac054b2e664
miao youdong <youdong.miao@sap.com>
2017-05-22 13:41:37 +0800

    build more meaningful data.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fakedataserver
 a fake data server with json-server
+
+
+how to build:
+
+run npm run mock.
--- generateMockData.js
+++ generateMockData.js
@@ -10,15 +10,27 @@ const zhenduan = ['感冒','骨折','发热','高血压','昏厥'];
 //let feiyong = ' ';
 const yaopin = '药品';
 const keshis = [];
-const yishengs = []
+const yishengs = [];
 
+
+const translation = {
+	hospital:"医院",
+	keshi:"科室",
+	yisheng:"医生",
+	zhenduan:"诊断",
+	time:"时间",
+	feiyong:"费用"
+};
 
 
 for(let i=0;i<5;i++){
-	keshis[i] = `${keshi}${i}`;
+	keshis[i] = `${keshi}${i}-${hospital[0]}`;
+	keshis[i+5] = `${keshi}${i}-${hospital[1]}`;
 	yishengs[i]=[];
+	yishengs[i+5] = [];
 	for(let j = 0; j< 10;j++){
-		yishengs[i][j]=`${yisheng}-${i}-${j}`;
+		yishengs[i][j]=`${yisheng}-${i}-${j}-${hospital[0]}`;
+		yishengs[i+5][j]=`${yisheng}-${i}-${j}-${hospital[1]}`;
 	}
 }
 
@@ -29,15 +41,18 @@ let fullData = [];
 
 
 for(let i = 0; i< 500; i++){
+	let hospitalRand = Math.random();
+	let hospitalChoice = hospitalRand>0.5?hospital[1]:hospital[0];
 	let keshiRand = Math.floor(Math.random()*5);
+	keshiRand = hospitalRand>0.5?keshiRand+5:keshiRand;
 	let time = new Date(Date.now()-Math.random()*10000000000);
 
 	fullData[i]={
 		id: i,
-		hospital: Math.random()>0.5?hospital[1]:hospital[0],
+		hospital: hospitalRand>0.5?hospital[1]:hospital[0],
 		keshi: keshis[keshiRand],
 		yisheng:yishengs[keshiRand][Math.floor(Math.random()*10)],
-		zhenduan: zhenduan[keshiRand],
+		zhenduan: zhenduan[Math.floor(Math.random()*5)],
 		time:time,
 		feiyong: Math.floor(Math.random()*1000),
 		yaopin: "TBD"
@@ -46,17 +61,18 @@ for(let i = 0; i< 500; i++){
 
 
 
-function byDimFeiyong(datas,...keys){
+function byDimFeiyong(datas,keys){
 	let byDim = [];
 	let unionkeys =[];
 	datas.map(data=>{
-		let unionkey = keys.map(key=>data[key]).join('/');
+		let fieldKeys = keys.map(key=>data[key]);
+		let unionkey = fieldKeys.join("/");
 		if(unionkeys.indexOf(unionkey)===-1){
 			unionkeys.push(unionkey);
 			byDim[unionkeys.length-1]={};
-			byDim[unionkeys.length-1]["key"] = unionkey;
+			byDim[unionkeys.length-1]['key'] = unionkey;
+			keys.forEach((key,index)=>byDim[unionkeys.length-1][key] = fieldKeys[index]);
 			byDim[unionkeys.length-1].feiyong= data.feiyong;
-
 		}else{
 			byDim[unionkeys.indexOf(unionkey)].feiyong+=data.feiyong;
 		}
@@ -65,27 +81,77 @@ function byDimFeiyong(datas,...keys){
 }
 
 
+function createMetadata(key, members,type){
+    return{
+        id:key,
+        fieldName:key,
+        name:translation[key],
+        members:members,
+        type:type
+    };
+}
 
-let byhospital = byDimFeiyong(fullData, "hospital");
-let bykeshi = byDimFeiyong(fullData, "keshi");
-let byyisheng = byDimFeiyong(fullData, "yisheng");
-let byzhenduan = byDimFeiyong(fullData, "zhenduan");
 
-let byhospitalbyyisheng = byDimFeiyong(fullData, "hospital","yisheng");
-let byhospitalbyyishengbyzhenduan = byDimFeiyong(fullData, "hospital","yisheng","zhenduan");
+// let byhospital = byDimFeiyong(fullData, "hospital");
+// let bykeshi = byDimFeiyong(fullData, "keshi");
+// let byyisheng = byDimFeiyong(fullData, "yisheng");
+// let byzhenduan = byDimFeiyong(fullData, "zhenduan");
 
+// let byhospitalbyyisheng = byDimFeiyong(fullData, "hospital","yisheng");
+// let byhospitalbyyishengbyzhenduan = byDimFeiyong(fullData, "hospital","yisheng","zhenduan");
+
+// let byhospitalbyzhenduan = byDimFeiyong(fullData, "hospital","zhenduan");
+// let byyishengbyzhenduan = byDimFeiyong(fullData, "yisheng","zhenduan");
+
+// const hospital= ['瑞金', '中山'];
+// const keshi = '科室';
+// const yisheng = '医生';
+// const zhenduan = ['感冒','骨折','发热','高血压','昏厥'];
+// //let shijian = '';
+// //let feiyong = ' ';
+// const yaopin = '药品';
+// const keshis = [];
+// const yishengs = []
+let metadata = [createMetadata('hospital',hospital,'Dimension'),createMetadata('keshi',keshis,'Dimension'),
+createMetadata('yisheng',yishengs.join(",").split(","),'Dimension'),createMetadata('zhenduan',zhenduan,'Dimension'),
+createMetadata('time',null,'Time'),createMetadata('feiyong',null,'Measure')];
+
+
+function buildParse(arr){
+	if(arr.length==1){
+		return ["","by"+arr[0]];
+	}
+	let parseList = buildParse(arr.slice(1,arr.length));
+	let parseList2 = parseList.map(l=>{
+		if(l){
+			return "by"+arr[0]+l;
+		}
+		return "by"+arr[0];
+	});
+	return [].concat(parseList,parseList2);
+}
 
 let output = {
 	data: fullData,
-	byhospital,
-	bykeshi,
-	byyisheng,
-	byzhenduan,
-	byhospitalbyyisheng,
-	byhospitalbyyishengbyzhenduan
+    metadata
 };
+	// byhospital,
+	// bykeshi,
+	// byyisheng,
+	// byzhenduan,
+	// byhospitalbyyisheng,
+	// byhospitalbyyishengbyzhenduan,
+	// byhospitalbyzhenduan,
+	// byyishengbyzhenduan,
+const sequence = ['hospital','keshi','yisheng','zhenduan'];
 
+let allSeq = buildParse(sequence);
 
+allSeq.forEach(seq=>{
+	if(seq){
+		output[seq] = byDimFeiyong(fullData,seq.split("by").filter(d=>d!==""));
+	}
+})
 
 
 //http://localhost:3333/byhospitalbyyishengbyzhenduan?key_like=%E5%8F%91%E7%83%AD
